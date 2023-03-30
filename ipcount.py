23a3730a161e55307b81d1504d30e24b7c8a3b43
OzzyHeredia <124816901+OzzyHeredia@users.noreply.github.com>
2023-03-29 21:16:23 -0400

    Add files via upload

--- ipcount.py
+++ ipcount.py
@@ -5,13 +5,14 @@
 import pyshark
 
 #Create a variable to open and read the file we are parsing.
-cap = pyshark.FileCapture("/home/ozzy/project/project4.pcap")
+cap = pyshark.FileCapture("/home/ozzy/project/project2.pcap")
 
 #Create a dictionary to store all the IPs and counts.
 ip_counts = {}
 
 #Loop through each packet in the capture.
 for packet in cap:
+    try:
 
 #Get the Source IPs and Destiantion IPs.
         ip_src = packet.ip.src
@@ -21,20 +22,21 @@ for packet in cap:
         if ip_src not in ip_counts:
             ip_counts[ip_src] = 1
         else:
-           ip_counts[ip_src] += 1
+            ip_counts[ip_src] += 1
     
 #Increment the count for the destination IP addresses.
         if ip_dst not in ip_counts:
-         ip_counts[ip_dst] = 1
+            ip_counts[ip_dst] = 1
         else:
             ip_counts[ip_dst] += 1
 
-for ip, count in ip_counts.items():
-
-#Print IP addresses and how often they populate.
-    print("IP Address:" + (ip) + "  Count:" + (str(count)))
-
 #Pass on any Attribute Errors.
+    except AttributeError:
+        pass
+#Print IP addresses and how often they populate.
+for ip, count in ip_counts.items():
+   print("IP Address:" + (ip) + "  Count:" + (str(count)))
+
 
 
 
